fa2f54f5d773cdc98d93cfbf92a2cad08f7457f0
yomuim <vdovin21@gmail.com>
2022-10-19 23:46:13 +0500

    update imports, style fixes

--- src/views/app-views/main/planner/index.js
+++ src/views/app-views/main/planner/index.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { Row, Col, Tabs, Space, Button, Upload } from 'antd';
-import { plannerTabsData } from './PlannerTabsData';
-import ItemCard from './ItemCard';
+import ItemCard from 'components/shared-components/ItemCard';
 import Planner from './Planner';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
 import utils from 'utils';
+import tabs from "assets/data/planner-tabs.data.json";
 
 const PlannerPage = () => {
   const [addedImages, setAddedImages] = useState([]);
@@ -23,13 +23,17 @@ const PlannerPage = () => {
     });
   };
 
-  const items = plannerTabsData.map((tab, index) => ({
+  const items = tabs.map((tab, index) => ({
     label: tab.title,
     key: index,
-    children: <Space>
-      {tab.content.map(({ img, title }) => <ItemCard onClick={() => handleAddImage(img)} key={img} img={img} title={title} />)}
-      </Space>
-  }));
+    children: (
+      <div style={{ overflowX: 'auto' }}>
+        <Space>
+          {tab.content.map(({ img, title }) => <ItemCard onClick={() => handleAddImage(img)} key={img} img={img} title={title} />)}
+        </Space>
+      </div>
+    )}
+  ));
 
   const handleDownload = () => utils.saveAsJSON(addedImages, "plan");
 
@@ -49,17 +53,15 @@ const PlannerPage = () => {
   return (
     <Row gutter={[16, 16]}>
       <Col xs={24} sm={12} md={12} lg={12}>
-        <Row gutter={16}>
-          <Tabs className='col-6' defaultActiveKey='1' items={items} />
-        </Row>
-        <Row gutter={16}>
-            <Space size="middle" wrap>
+          <Row gutter={[16, 16]}>
+            <Tabs className='col-6' defaultActiveKey='1' items={items} />
+            <Space size="middle" wrap className='flex-grow-1 justify-content-center justify-content-lg-start justify-content-xl-start '>
               <Button type="primary" onClick={handleDownload}>Скачать планировку</Button>
               <Upload accept='application/JSON' beforeUpload={handleUpload}>
                 <Button onClick={handleUpload}>Загрузить из файла</Button>
               </Upload>
             </Space>
-        </Row>
+          </Row>
       </Col>
       <Col xs={24} sm={12} md={12} lg={12}>
         {isTouch && (
